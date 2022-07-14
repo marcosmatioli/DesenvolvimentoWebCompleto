786b7a7ddcc0fd2b1d82ce64e04203651e3404f1
marcosmatioli <100784085+marcosmatioli@users.noreply.github.com>
2022-07-14 12:53:33 -0300

    Aula de javascript variaveis

---
+++ JavaScript/js/meu_script2.js
@@ -0,0 +1,7 @@
+/* então vem antes de tudo aqui depende de onde vc coloca no seu html se estiver antes, isso aqui
+vai ser feito antes e se estiver depois vai ser feito depois
+basicamente alert na pagina web*/
+
+//alert('olá mundo') 
+//pega o elemento dom e coloca o valor oi no id
+document.getElementById('nome').value = 'Oi'
