916737a07acc1c6e0ce1c982f1fca3c4be45c40e
marcosmatioli <100784085+marcosmatioli@users.noreply.github.com>
2022-08-08 17:44:05 -0300

    filter no app orçamento em produção

--- Projetos/Projeto App Orçamento Pessoal/js/app.js
+++ Projetos/Projeto App Orçamento Pessoal/js/app.js
@@ -41,6 +41,52 @@ class Bd {
         /** aqui fica o controlador do proximo item \/*/
         localStorage.setItem('id', id)
     }
+    recuperarTodosRegistro(){
+        //array de despesa
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+        /**recupera todas as despesas cadastradas em localStorage */
+        for (let i = 1;i <= id; i++){
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver indices que foram pulados/removidos
+            if(despesa === null)
+            {
+                continue /**ele vai para a proxima iteração */
+            }
+            despesas.push(despesa)
+        }
+        return despesas
+    }
+    pesquisar(despesa){
+        let despesasFiltradas = Array()
+        despesasFiltradas = this.recuperarTodosRegistro()
+        console.log(despesa)
+        console.log(despesasFiltradas)
+
+        //ano
+        /**aqui o despesasFiltradas recebe a array só que agora com o filtro  nele */
+        if(despesa.ano != ''){
+            console.log('Filtro de ano')
+           despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        
+        //mes
+        if(despesa.ano != ''){
+            console.log('Filtro de mes')
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+         }
+         console.log(despesasFiltradas)
+        //dia
+
+        //descrição
+
+        //valor
+
+        //tipo
+
+        }
 }
 
 let bd = new Bd()
@@ -62,12 +108,74 @@ function cadastrarDespesa() {
         valor.value
     )
     if (despesa.validarDados()) {
-        //dialog de sucesso
         console.log('Dados inseridos com sucesso')
-        // bd.gravar(despesa)
+        bd.gravar(despesa)
+        //dialog de sucesso em jquery #
+        $('#modalRegistraDespesa').modal('show')
+        document.getElementById('exampleModalLabel').innerHTML= 'Registro inserido com sucesso'
+        document.getElementById('modal-text').innerHTML = 'Despesa foi cadastrada com sucesso!'
+        document.getElementById('btnModal').innerHTML = 'Voltar'
+        document.getElementById('cor-modal-header').className = 'modal-header text-success'
+        document.getElementById('btnModal').className = 'btn btn-primary btn-success'
+        /**limpar os campos em caso de sucesso para poder
+         * fazer uma outra gravação de forma mais rapida
+         */
+        mes.value = ''
+        ano.value = ''
+        tipo.value = ''
+        dia.value = ''
+        valor.value = ''
+        descricao.value = ''
+
+        /** jeito que eu fiz o desafio mas que poderia ter sido mais fácil
+         * 
+         * document.getElementById('mes').value = ''
+        document.getElementById('ano').value = ''
+        document.getElementById('tipo').value = ''
+        document.getElementById('dia').value = ''
+        document.getElementById('valor').value = ''
+        document.getElementById('descricao').value = ''
+        */
+        
     } else {
-        //dialog de erro
+        //dialog de erro em jquery $
+        $('#modalRegistraDespesa').modal('show')
+        document.getElementById('exampleModalLabel').innerHTML= 'Erro na inclusão de despesa'
+        document.getElementById('modal-text').innerHTML = 'Existem campos obrigatórios que não foram preenchidos'
+        document.getElementById('btnModal').innerHTML = 'Voltar e Corrigir'
+        document.getElementById('cor-modal-header').className = 'modal-header text-danger'
+        document.getElementById('btnModal').className = 'btn btn-primary btn-danger'
         console.log('Dados invalidos')
     }
-
+}
+function carregaListaDespesa(){
+    let despesas = Array()
+    despesas =  bd.recuperarTodosRegistro()
+    /**estamos selecionando o elemento tbody da tabela */
+    let listaDespesa = document.getElementById('listaDespesa')
+    despesas.forEach(function (d){
+        console.log(d)
+        /**criando as linhas 1 linha */
+        let linha = listaDespesa.insertRow()
+        /**criando as colunas 4 colunas */
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        linha.insertCell(1).innerHTML = `${d.tipo}`
+        /** aqui o tipo era um número ai o cara fez um switch aqui
+         * convertendo a string ou não em int ficaria ao critério do dev
+         * mas eu achei mais viavel mudar o value no html
+         * então o que vier de lá já está certo!
+         */
+        linha.insertCell(2).innerHTML = `${d.descricao}`
+        linha.insertCell(3).innerHTML = `${d.valor}`
+    })
+}
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+    let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor)
+    bd.pesquisar(despesa)
 }
